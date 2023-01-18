bfc9aaa81c6a9a277051009209cf6e3158e64d12
vnratc <105481726+vnratc@users.noreply.github.com>
2023-01-19 00:25:15 +0400

    Update app.py

--- app.py
+++ app.py
@@ -24,8 +24,8 @@ Session(app)
 db = SQL("sqlite:///finance.db")
 
 # Make sure API key is set
-if not os.environ.get("API_KEY"):
-    raise RuntimeError("API_KEY not set")
+# if not os.environ.get("API_KEY"):
+  #   raise RuntimeError("API_KEY not set")
 
 now = datetime.now()
 date_time = now.strftime("%Y-%m-%d %H:%M:%S")
